e65a0277ee37b1f4c5cb96cc15125a37b71fe533
siewchoo <snowpod@gmail.com>
2025-06-22 00:57:47 +0800

    Add hypertext linking for letter

---
+++ public/OntologyOne/config.js
@@ -0,0 +1,5 @@
+window.APP_LINKS = {
+  app: "https://ontologyone-frontend-qm41.onrender.com/?tab=app",
+  chatbot: "https://ontologyone-frontend-qm41.onrender.com/?tab=chatbot",
+  blog: "https://thinklog.netlify.app",
+};
